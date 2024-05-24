e03aaea9c756d9e543c4497a072e4eb7c7bc5ed8
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-05-24 15:49:02 +0200

    feat: esercizio completato

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routePosts = require("./Controller/posts");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(`<h1>Benvenuto nel mio Blog!</h1>`);
 });
+
+app.get("/posts", routePosts);
 
 app.listen(port, () => {
   console.log(`Server http://localhost:${port}`);
---
+++ postsDb.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    title: "Il mio primo post",
+    content: "Questo è il contenuto del mio primo post.",
+    image: "primo-post.jpg",
+    tags: ["personale", "inizio", "blog"],
+  },
+  {
+    title: "Viaggio in Italia",
+    content: "Racconto del mio meraviglioso viaggio in Italia.",
+    image: "italia.jpg",
+    tags: ["viaggi", "italia", "avventura"],
+  },
+  {
+    title: "La cucina italiana",
+    content: "I miei piatti preferiti della cucina italiana.",
+    image: "cucina-italiana.jpg",
+    tags: ["cucina", "italia", "ricette"],
+  },
+  {
+    title: "Tecnologia e Innovazione",
+    content: "Le ultime novità dal mondo della tecnologia.",
+    image: "tecnologia.jpg",
+    tags: ["tecnologia", "innovazione", "futuro"],
+  },
+  {
+    title: "Guida alla programmazione",
+    content: "I migliori consigli per iniziare a programmare.",
+    image: "programmazione.jpg",
+    tags: ["programmazione", "guida", "sviluppo"],
+  },
+];
+
+module.exports = posts;
